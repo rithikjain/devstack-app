988c98c1fbdbe269b9a49b96d3979d822a0982f0
Rithik Jain <rithik.jain3006@gmail.com>
2020-09-12 02:58:35 +0530

    Google auth working

--- lib/src/presentation/screens/auth/auth_screen.dart
+++ lib/src/presentation/screens/auth/auth_screen.dart
@@ -1,7 +1,10 @@
 import 'package:devtalks/src/presentation/animations/show_up.dart';
+import 'package:devtalks/src/presentation/screens/main/base_screen.dart';
 import 'package:devtalks/src/presentation/themes/text_styles.dart';
 import 'package:devtalks/src/presentation/widgets/bg_gradient.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthScreen extends StatefulWidget {
   static const routename = "/auth";
@@ -12,6 +15,58 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   bool _isLoading = false;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<void> _signInWithGoogle(BuildContext context) async {
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+      final GoogleSignInAccount googleSignInAccount =
+          await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      final UserCredential authResult =
+          await _auth.signInWithCredential(credential);
+      final User user = authResult.user;
+
+      if (user != null) {
+        final User currentUser = _auth.currentUser;
+        print(currentUser.email);
+        Navigator.pushNamedAndRemoveUntil(
+          context,
+          BaseScreen.routename,
+          (route) => false,
+        );
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    } catch (err) {
+      setState(() {
+        _isLoading = false;
+      });
+      final snackBar = SnackBar(
+        content: Text(
+          'Something went wrong!',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: Colors.red,
+      );
+      Scaffold.of(context).showSnackBar(snackBar);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,73 +80,79 @@ class _AuthScreenState extends State<AuthScreen> {
           child: Image.asset("assets/images/devstack.png"),
         ),
       ),
-      body: Stack(
-        children: [
-          BgGradient(),
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: ShowUp(
-              delay: Duration(milliseconds: 600),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: RaisedButton(
-                      onPressed: () {},
-                      color: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: (!_isLoading)
-                          ? Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text('Login with Google'),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Image.network(
-                                  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png",
-                                  fit: BoxFit.contain,
-                                  height: 24,
-                                )
-                              ],
-                            )
-                          : Container(
-                              height: 35,
-                              width: 35,
-                              child: CircularProgressIndicator(),
-                            ),
-                    ),
-                  ),
-                  SizedBox(height: 100),
-                  RichText(
-                    text: TextSpan(
-                      text: 'Click here to know about ',
-                      style: WhiteText.copyWith(
-                        fontSize: 17,
-                        decoration: TextDecoration.underline,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: 'DevTalks',
-                          style: PalePinkText.copyWith(
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline,
-                          ),
+      body: Builder(
+        builder: (context) => Stack(
+          children: [
+            BgGradient(),
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: ShowUp(
+                delay: Duration(milliseconds: 600),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: RaisedButton(
+                        onPressed: () async {
+                          if (!_isLoading) {
+                            await _signInWithGoogle(context);
+                          }
+                        },
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                      ],
+                        child: (!_isLoading)
+                            ? Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text('Login with Google'),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Image.network(
+                                    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png",
+                                    fit: BoxFit.contain,
+                                    height: 24,
+                                  )
+                                ],
+                              )
+                            : Container(
+                                height: 35,
+                                width: 35,
+                                child: CircularProgressIndicator(),
+                              ),
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 100),
+                    RichText(
+                      text: TextSpan(
+                        text: 'Click here to know about ',
+                        style: WhiteText.copyWith(
+                          fontSize: 17,
+                          decoration: TextDecoration.underline,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'DevTalks',
+                            style: PalePinkText.copyWith(
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
