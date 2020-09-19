70cd262950fb5d00b272ca3c7b1dff5c0298b077
Rithik Jain <rithik.jain3006@gmail.com>
2020-09-20 00:52:17 +0530

    Network: Fetching questions from firebase

--- lib/src/presentation/screens/main/questions_screen.dart
+++ lib/src/presentation/screens/main/questions_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:devtalks/src/presentation/animations/show_up.dart';
 import 'package:devtalks/src/presentation/themes/themes.dart';
 import 'package:devtalks/src/presentation/widgets/question_card.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +7,10 @@ import 'package:flutter/material.dart';
 class QuestionsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Query _questions = FirebaseFirestore.instance
+        .collection("questions")
+        .orderBy("upvotes", descending: true);
+
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: Column(
@@ -23,15 +29,33 @@ class QuestionsScreen extends StatelessWidget {
             ),
           ),
           SizedBox(height: 8),
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) => QuestionCard(
-                question: "What is DevStack",
-                description: "Devstack is an event oragnised by DSC VIT.",
-                upvotes: 10,
-              ),
-              itemCount: 5,
-            ),
+          StreamBuilder<QuerySnapshot>(
+            stream: _questions.snapshots(includeMetadataChanges: true),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return Center(child: Text("Something went wrong"));
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Container();
+              }
+              snapshot.data.docs.forEach((element) {
+                print(element.data());
+              });
+              return Expanded(
+                child: ShowUp(
+                  delay: Duration(milliseconds: 200),
+                  child: ListView.builder(
+                    itemBuilder: (context, index) => QuestionCard(
+                      question: snapshot.data.docs[index].data()["title"],
+                      description:
+                          snapshot.data.docs[index].data()["description"],
+                      upvotes: snapshot.data.docs[index].data()["upvotes"],
+                    ),
+                    itemCount: snapshot.data.docs.length,
+                  ),
+                ),
+              );
+            },
           ),
           SizedBox(height: 8),
         ],
